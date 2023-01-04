50b58567bfcdf79a855afcd54787a5186327826e
Julio aguilar <julioa.m777@hotmail.com>
2023-01-03 18:38:30 -0600

    [ADD] redux - add people and add favorites

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 
+import { Provider } from 'react-redux'
 import './App.css'
 import { Navbar } from './components'
 import { Home } from './pages'
+import store from './redux/store'
 import { LayoutContainer } from './styledComponents'
 
 function App() {
 
   return (
-    <>
+    <Provider store={store}>
       <Navbar />
       <LayoutContainer>
         <Home />
       </LayoutContainer>
-    </>
+    </Provider>
   )
 }
 
---
+++ src/redux/slices/favorites.ts
@@ -0,0 +1,20 @@
+import { LocalStorageTypes, Person } from '@/models';
+import { getLocalStorage, setLocalStorage } from '@/utilities';
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState: Person[] = [];
+
+export const favoriteSlice = createSlice({
+  name: 'favorite',
+  initialState: getLocalStorage(LocalStorageTypes.FAVORITES)
+    ? JSON.parse(getLocalStorage(LocalStorageTypes.FAVORITES) as string)
+    : initialState,
+  reducers: {
+    addFavorite: (state, action) => {
+      setLocalStorage(LocalStorageTypes.FAVORITES, state);
+      return action.payload;
+    },
+  },
+});
+
+export const { addFavorite } = favoriteSlice.actions;
